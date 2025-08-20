dd81d4b5521666b4872a1370d39a688ef894811d
groninge <groninge@gmail.com>
2025-08-20 17:08:55 +0200

    remove folders

--- modules/pool/detail/Pool.tsx
+++ modules/pool/detail/Pool.tsx
@@ -3,7 +3,6 @@ import PoolHeader from '~/modules/pool/detail/components/PoolHeader';
 import { PoolComposition } from '~/modules/pool/detail/components/composition/PoolComposition';
 import PoolStats from './components/stats/PoolStats';
 import { PoolDetailCharts } from '~/modules/pool/detail/components/PoolDetailCharts';
-import { PoolInvestModal } from '~/modules/pool/invest/PoolInvestModal';
 import { PoolWithdrawModal } from '~/modules/pool/withdraw/PoolWithdrawModal';
 import { usePool } from '~/modules/pool/lib/usePool';
 import { useNetworkConfig } from '~/lib/global/useNetworkConfig';
